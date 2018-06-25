367d92b22ac12628536864aeb728105c51feb502
Mathieu Chartier <mathieuc@google.com>
2018-06-25 04:07:02 +0200

    beryllium: Speed services and wifi-service to reduce RAM and storage
    Change-Id: I1d907b24cb2760ddd9cc65ac4335730c8e61a0a3

--- sdm845.mk
+++ sdm845.mk
@@ -337,6 +337,9 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/xiaomi \
     vendor/qcom/opensource/usb/etc
 
+# Speed services and wifi-service to reduce RAM and storage.
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed
+
 # Telephony
 PRODUCT_PACKAGES += \
     ims-ext-common \
